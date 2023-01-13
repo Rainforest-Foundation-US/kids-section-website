b4fe9b101efe92dfe394d93ee31c1a42353afd38
Rowin Hernandez <rowinkaleb@gmail.com>
2023-01-13 23:52:10 +0000

    Setup TailwindCSS + Prettier and initial style guide config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "@next/font/google";
+
+const fontFamily = Poppins({
+  subsets: ["latin"],
+  variable: "--app-font",
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${fontFamily.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
