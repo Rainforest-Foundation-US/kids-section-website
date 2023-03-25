40a94735318e171e5057b535e785fcfe2f1a5e10
Rowin Hernandez <rowinkaleb@gmail.com>
2023-03-25 14:07:57 +0000

    Add title to pages and default website description + style fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="The RFUS Kids' Corner is a place to discover the fascinating secrets of the Amazon and help us protect its animals, plants and communities."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
