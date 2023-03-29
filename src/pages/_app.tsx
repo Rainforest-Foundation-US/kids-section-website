d6bab24a69569d6fc2a39061a6a028154164a75c
Rowin Hernandez <rowinkaleb@gmail.com>
2023-03-29 09:10:51 +0100

    Fix meta tag on _document warning

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "@next/font/google";
+import Head from "next/head";
 
 const fontFamily = Poppins({
   subsets: ["latin"],
@@ -10,8 +11,18 @@ const fontFamily = Poppins({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${fontFamily.variable} font-sans`}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="The RFUS Kids' Corner is a place to discover the fascinating secrets of the Amazon and help us protect its animals, plants and communities."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <div className={`${fontFamily.variable} font-sans`}>
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta
-          name="description"
-          content="The RFUS Kids' Corner is a place to discover the fascinating secrets of the Amazon and help us protect its animals, plants and communities."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
