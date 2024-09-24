1307f03643d345e098914a309553d9a9d7d12512
Kokan Malenko <kokan_malenko@hotmail.com>
2024-09-24 15:07:37 +0200

    Format code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // TODO: add once we move images to Sanity
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "cdn.sanity.io",
+  //     },
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;
