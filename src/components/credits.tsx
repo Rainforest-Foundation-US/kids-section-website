f4b0c7bb568548add31006d6ee7c58894699bf6e
Kokan Malenko <kokan_malenko@outlook.com>
2024-12-03 15:45:55 +0100

    Add: SSG

--- src/components/credits.tsx
+++ src/components/credits.tsx
@@ -1,63 +1,16 @@
-import clsx from "clsx";
 import React from "react";
 
-// Define the structure of a contributor
 interface Contributor {
   name: string;
   role?: string;
 }
 
-// Define the structure of a category
-interface CategoryProps {
+export interface ContributorData {
   title: string;
   contributors: Contributor[];
   className?: string;
 }
 
-const contributorData = [
-  {
-    title: "Development",
-    contributors: [
-      { name: "Rowin Hernández", role: "Lead developer" },
-      { name: "Kokan Malenko", role: "Lead developer" },
-      { name: "Kari Hernández", role: "Developer" },
-      { name: "Lokesh Pathrabe", role: "Developer" },
-      { name: "Ali Aizaz", role: "Developer" },
-      { name: "Mohammed Agboola", role: "Developer" },
-      { name: "Amaan Salheen", role: "Developer" },
-      { name: "Nelio Carneiro", role: "Developer" },
-    ],
-  },
-  {
-    title: "Project Management Support",
-    contributors: [{ name: "Simone Theeboom" }],
-  },
-  {
-    title: "Content Development",
-    contributors: [
-      { name: "Rainforest Foundation US" },
-      { name: "Jeremy Kundtz " },
-    ],
-  },
-  {
-    title: "UX Design",
-    contributors: [
-      { name: "Darwin Álvarez", role: "Lead UX designer" },
-      { name: "Salman Syed Muhammad", role: "UX designer" },
-    ],
-  },
-  {
-    title: "Assets",
-    contributors: [
-      { name: "Golshid Yazdi", role: "Illustrations" },
-      { name: "Simon Dures Productions", role: "Video" },
-      { name: "Rainforest Foundation US", role: "Images" },
-      { name: "Adobe Stock", role: "Images" },
-    ],
-  },
-];
-
-// Random rotation and translation classes for playful positioning
 const positionClasses = [
   "md:rotate-[-2deg]",
   "md:rotate-[2deg]",
@@ -66,8 +19,7 @@ const positionClasses = [
   "md:rotate-[-1deg]",
 ];
 
-// Category component to render a single category of contributors
-function Category({ title, contributors, className }: CategoryProps) {
+function Category({ title, contributors, className }: ContributorData) {
   return (
     <div
       className={`rounded-xl border border-primary-600 bg-primary-500 p-6 text-primary-800 shadow-app-lg shadow-shadow-green ${className} max-w-96`}
@@ -87,10 +39,11 @@ function Category({ title, contributors, className }: CategoryProps) {
   );
 }
 
-// Main Credits Page Component
-function CreditsPage() {
-  // Comprehensive contributor data
-
+function CreditsPage({
+  contributorData,
+}: {
+  contributorData: ContributorData[];
+}) {
   return (
     <div className="bg-emerald-950 text-white">
       <h1 className="text-emerald-100 mb-12 text-center text-4xl font-extrabold">
--- src/pages/credits.tsx
+++ src/pages/credits.tsx
@@ -2,11 +2,66 @@ import { NavBar } from "@/components/layout/nav";
 import Head from "next/head";
 
 import { Footer } from "@/components/layout/footer";
-import CreditsPage from "@/components/credits";
+import CreditsPage, { ContributorData } from "@/components/credits";
 import { RegularSection } from "@/components/sections/regular-section";
 import { SectionContent } from "@/components/content/section-content";
 
-export default function CreditsRoute() {
+export async function getStaticProps() {
+  const contributorData = [
+    {
+      title: "Development",
+      contributors: [
+        { name: "Rowin Hernández", role: "Lead developer" },
+        { name: "Kokan Malenko", role: "Lead developer" },
+        { name: "Kari Hernández", role: "Developer" },
+        { name: "Lokesh Pathrabe", role: "Developer" },
+        { name: "Ali Aizaz", role: "Developer" },
+        { name: "Mohammed Agboola", role: "Developer" },
+        { name: "Amaan Salheen", role: "Developer" },
+        { name: "Nelio Carneiro", role: "Developer" },
+      ],
+    },
+    {
+      title: "Project Management Support",
+      contributors: [{ name: "Simone Theeboom" }],
+    },
+    {
+      title: "Content Development",
+      contributors: [
+        { name: "Rainforest Foundation US" },
+        { name: "Jeremy Kundtz " },
+      ],
+    },
+    {
+      title: "UX Design",
+      contributors: [
+        { name: "Darwin Álvarez", role: "Lead UX designer" },
+        { name: "Salman Syed Muhammad", role: "UX designer" },
+      ],
+    },
+    {
+      title: "Assets",
+      contributors: [
+        { name: "Golshid Yazdi", role: "Illustrations" },
+        { name: "Simon Dures Productions", role: "Video" },
+        { name: "Rainforest Foundation US", role: "Images" },
+        { name: "Adobe Stock", role: "Images" },
+      ],
+    },
+  ];
+
+  return {
+    props: {
+      contributorData,
+    },
+  };
+}
+
+export default function CreditsRoute({
+  contributorData,
+}: {
+  contributorData: ContributorData[];
+}) {
   return (
     <>
       <Head>
@@ -19,7 +74,7 @@ export default function CreditsRoute() {
 
           <div className="relative">
             <SectionContent>
-              <CreditsPage />
+              <CreditsPage contributorData={contributorData} />
             </SectionContent>
           </div>
         </RegularSection>
