89f86644ded1c5c6322c53e950a97f7fadb544b5
Kokan Malenko <kokan_malenko@outlook.com>
2025-07-28 16:33:50 +0200

    Fix: illustrations for laptop screens

--- src/styles/illustration-styles.ts
+++ src/styles/illustration-styles.ts
@@ -1,5 +1,5 @@
 const commonIllustrationStyles =
-  "absolute -z-10 scale-[35%] sm:scale-50 md:scale-75 xl:scale-110 object-scale-down";
+  "absolute -z-10 scale-[35%] sm:scale-50 md:scale-75 2xl:scale-110 object-scale-down";
 
 export const bottomLeftIllustrationStyles = `bottom-0 left-0 origin-bottom-left ${commonIllustrationStyles}`;
 
